cee977c1b9c0cb3d657d91580b8b2b1af36333d2
kaneda0911 <51037928+kaneda0911@users.noreply.github.com>
2019-06-02 20:12:47 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+$(function(){
+ var $list = $('.todoList');
+ var $input = $('.todoInput');
+
+ function addTodo(text){
+  var $li = $('<li>');
+  var $text = $('<span class="text">').text(text);
+  var $checkbox = $('<input type="checkbox">');
+  var $remove =$('<span class="remove">削除</span>');
+  $li.append($checkbox).append($text).append($remove);
+
+  $checkbox.click(function(){
+    if($(this).is(':checked')){
+      $li.addClass('complete');
+    }
+    else{
+      $li.removeClass('complete');
+    }
+  });
+
+  $remove.click(function(){
+    if(windows.confirm('削除してよろしいですか？')){
+      $li.remove();
+      }
+    });
+
+  $list.append($li);
+ }
+
+ $('.todoForm').bind('submit',function(event){
+   event.preventDefault();
+
+   var text = $input.val();
+
+   addTodo(text);
+
+   $input.val('');
+ });
+});
